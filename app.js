70e290db8ddd2c394671cdcd6d07388b0ed6fc9a
rishab2113 <rishab_nayak@yahoo.com>
2018-05-19 15:14:10 -0400

    nxt

--- app.js
+++ app.js
@@ -8,22 +8,21 @@ app.use(express.static(__dirname + '/views'));
 app.engine('html', require('ejs').renderFile);
 app.get('/', (req, res) => {
   res.render('homepage.html');
+  io.on('connection',function(socket){
+      socket.on('sum', function(sum, CANVAS_WIDTH, CANVAS_HEIGHT){
+        var avg;
+        var out;
+        var conc;
+        avg = sum/(CANVAS_WIDTH*CANVAS_HEIGHT);
+        conc = (avg-0.5557)/0.3883;
+        var outconc = conc.toFixed(2);
+        out = "The concentration is "+outconc+"mg/L"
+        io.emit('response',out);
+      });
+  });
 });
-io.on('connection',function(socket){
-  console.log('user connected');
-    socket.on('disconnect',() => {
-      console.log('user disconnected');
-    });
-    socket.on('sum', function(sum, CANVAS_WIDTH, CANVAS_HEIGHT){
-      var avg;
-      var out;
-      var conc;
-      avg = sum/(CANVAS_WIDTH*CANVAS_HEIGHT);
-      conc = (avg-0.5557)/0.3883;
-      var outconc = conc.toFixed(2);
-      out = "The concentration is "+outconc+"mg/L"
-      io.emit('response',out);
-    });
+app.get('/results', (req, res) => {
+  res.render('results.html');
 });
 http.listen(8080, () => {
   console.log("View at localhost:8080");
