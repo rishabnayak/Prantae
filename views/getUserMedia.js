8661c81c8365b189fadf203339a2e288b94d6b13
rishab2113 <rishab@bu.edu>
2019-04-09 05:14:30 -0400

    minor

--- views/getUserMedia.js
+++ views/getUserMedia.js
@@ -4,25 +4,6 @@
   var video = document.querySelector('video'), canvas;
   canvas = document.getElementById('canvas');
   // use MediaDevices API
-  function dataURItoBlob(dataURI) {
-      // convert base64/URLEncoded data component to raw binary data held in a string
-      var byteString;
-      if (dataURI.split(',')[0].indexOf('base64') >= 0)
-          byteString = atob(dataURI.split(',')[1]);
-      else
-          byteString = unescape(dataURI.split(',')[1]);
-
-      // separate out the mime component
-      var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0];
-
-      // write the bytes of the string to a typed array
-      var ia = new Uint8Array(byteString.length);
-      for (var i = 0; i < byteString.length; i++) {
-          ia[i] = byteString.charCodeAt(i);
-      }
-
-      return new Blob([ia], {type:mimeString});
-  }
   // docs: https://developer.mozilla.org/en-US/docs/Web/API/MediaDevices/getUserMedia
   if (navigator.mediaDevices) {
     // access the web cam
